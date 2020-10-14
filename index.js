f479941d28eed49bf68be4b44059cab250c58686
Sebastian Jerratsch <sebastian.jerratsch@gmail.com>
2020-10-14 09:58:15 +0200

    allow registering in global NodeJs object for cross-project mocking

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ module.exports.jestMocker = (function(){
         throw new Error("Not yet implemented.");
     };
 
-    const mock = function(moduleName, factory){
+    const mock = function(moduleName, factory, registerInGlobalObj){
         if(!moduleName || !typeof(moduleName) === "string" || !moduleName.length){
             throw new Error("No module-name specified. Please specify module name either as node-module name or absolute path.");
         }
         if(factory && typeof(factory) === "function"){
             _mockFactories[moduleName] = factory;
+            if(registerInGlobalObj){
+                global.JESTLIKEMOCK_FACTORIES = global.JESTLIKEMOCK_FACTORIES || {}
+                global.JESTLIKEMOCK_FACTORIES[moduleName] = _mockFactories[moduleName];
+            }
         }
     };
 
@@ -23,8 +27,16 @@ module.exports.jestMocker = (function(){
         if(_mockFactories.hasOwnProperty(moduleName)){
             delete _mockFactories[moduleName];
 
+            if((global.JESTLIKEMOCK_FACTORIES || {}).hasOwnProperty(moduleName)){
+                delete global.JESTLIKEMOCK_FACTORIES[moduleName];
+            }
+
             if(_mockRegistry.hasOwnProperty(moduleName)){
                 delete _mockRegistry[moduleName];   
+
+                if((global.JESTLIKEMOCK_REGISTRY || {}).hasOwnProperty(moduleName)){
+                    delete global.JESTLIKEMOCK_REGISTRY[moduleName];
+                }
             }
         }
     };
@@ -35,7 +47,7 @@ module.exports.jestMocker = (function(){
         unmock
     }
 })();
-module.exports.default = function(_requireValue){
+module.exports.default = function(_requireValue, falseifnotfound){
 
     if(_requireValue){
         if (_mockFactories.hasOwnProperty(_requireValue)) {
@@ -45,8 +57,19 @@ module.exports.default = function(_requireValue){
 
             return (_mockRegistry[_requireValue] = _mockFactories[_requireValue]());
         }
-        else{
+        else if ((global.JESTLIKEMOCK_FACTORIES || {}).hasOwnProperty(_requireValue)) {
+            if((global.JESTLIKEMOCK_REGISTRY || {}).hasOwnProperty(_requireValue)){
+                return global.JESTLIKEMOCK_REGISTRY[_requireValue];
+            }
+
+            global.JESTLIKEMOCK_REGISTRY = global.JESTLIKEMOCK_REGISTRY || {};
+            return (global.JESTLIKEMOCK_REGISTRY[_requireValue] = global.JESTLIKEMOCK_FACTORIES[_requireValue]());
+        }
+        else if(!falseifnotfound){
             return require(_requireValue);
+        }
+        else {
+            return false;
         }
     }
 
